f5b441cf029fed048166b01450bd8a6f6cb85e4f
Noan <180792042+MrCheshir3@users.noreply.github.com>
2025-05-16 09:34:56 +0100

    escolhado computador

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
+// console.log(); para dar resposta no console
+
 function getComputerChoice() {
-    const choices = ['pedra', 'papel', 'tesoura'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+    numRD = Math.random();
+
+
+
+    if (numRD < 0.34) {
+        return "rock";
+    } else if (numRD <= 0.67) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
+
+console.log(getComputerChoice() );
